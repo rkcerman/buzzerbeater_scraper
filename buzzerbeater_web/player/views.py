53149f4c2006efc43eec67b0e9baaa26ea619937
rkcerman <rkaliarik@gmail.com>
2018-07-20 17:44:07 +0200

    /player/id/overview now showing basic info

--- buzzerbeater_web/player/views.py
+++ buzzerbeater_web/player/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Players
+from .models import Players, PlayerSkills
 
 
 def index(request):
@@ -12,9 +12,28 @@ def index(request):
     }
     return render(request, 'player/index.html', context)
 
+# Returns player overview
 def overview(request, player_id):
     player = Players.objects.get(id=player_id)
+    team = player.team
+    skills = PlayerSkills.objects.filter(player=player_id)
+
+    player_skills = {}
+    for skill in skills:
+        player_skills[skill.skill.replace(' ', '_').replace('.', '').lower()] = skill.value
+
+    guard_skill_points = player_skills['jump_shot'] + player_skills['jump_range'] + player_skills['outside_def'] \
+                        + player_skills['handling'] + player_skills['driving'] + player_skills['passing']
+    forward_skill_points = player_skills['inside_shot'] + player_skills['inside_def'] \
+                           + player_skills['rebounding'] + player_skills['shot_blocking']
+    tsp = guard_skill_points + forward_skill_points
+
     context = {
         'player': player,
+        'team': team,
+        'skills': player_skills,
+        'gsp': guard_skill_points,
+        'fsp': forward_skill_points,
+        'tsp': tsp,
     }
-    return render(request, player)
+    return render(request, 'player/player_overview.html', context)
