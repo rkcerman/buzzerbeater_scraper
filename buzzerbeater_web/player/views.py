c62c93cb610bbc95e033cdf5683b972d2fe05077
rkcerman <rkaliarik@gmail.com>
2018-07-20 15:53:18 +0200

    Added more files and changes to buzzerbeater_web

--- buzzerbeater_web/player/views.py
+++ buzzerbeater_web/player/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Players
 
 
 def index(request):
-    return HttpResponse("BB Players app")
+    latest_players_list = Players.objects.order_by('-last_update_at')[:10]
+    context = {
+        'latest_players_list': latest_players_list,
+    }
+    return render(request, 'player/index.html', context)
 
 def overview(request, player_id):
-    response = 'Player id: %s.'
-    return HttpResponse(response % player_id)
+    player = Players.objects.get(id=player_id)
+    context = {
+        'player': player,
+    }
+    return render(request, player)
