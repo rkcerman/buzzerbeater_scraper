7a5e4a4883b69fd603a70fc18748ec410fa6387a
Richard Kaliarik <rkaliarik@gmail.com>
2018-09-18 16:20:14 +0200

    Added django-rest-framework to the project.

--- buzzerbeater_web/buzzerbeater_web/urls.py
+++ buzzerbeater_web/buzzerbeater_web/urls.py
@@ -14,9 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.models import User
+from django.urls import path
+from django.conf.urls import include, url
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     path('bbstats/', include('bbstats.urls')),
     path('admin/', admin.site.urls),
+    url(r'^api', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
