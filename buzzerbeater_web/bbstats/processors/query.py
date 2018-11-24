5729ba479f7037d14809dbae7cb8e2beef4da8eb
Richard Kaliarik <rkaliarik@gmail.com>
2018-11-24 13:09:25 +0100

    Removed redis integration.

--- buzzerbeater_web/bbstats/processors/query.py
+++ buzzerbeater_web/bbstats/processors/query.py
@@ -1,33 +1,27 @@
-import redis
 from django.db.models import Q
 
 from bbstats.models import Matches, Boxscores, Teams, PlayerSkills, ScoreTables
 from collections import Counter
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-
 
 # TODO get this to process.py
-# Fetches the schedule from the DB, or if available from redis
+# Fetches the schedule from the DB
 def get_schedule(team, season):
     team_id = team.id
     schedule_with_bs = []
 
-    # Let's check if there is an existing schedule in redis
-    # If not, it fetches the matches from the DB and creates redis list
     schedule = Matches.objects.filter(season=season)\
         .filter(Q(away_team_id=team_id) | Q(home_team_id=team_id))\
         .order_by('match_date')
 
     # Append boxscore to each match and tuple them together
-    boxscore = None
-    end_score = ''
     for match in schedule:
         try:
             boxscore = get_boxscore(match.id)
             end_score = match_end_score(match.id)
         except Boxscores.DoesNotExist:
-            pass
+            boxscore = None
+            end_score = ''
         finally:
             match_bs = (match, boxscore, end_score)
             schedule_with_bs.append(match_bs)
@@ -54,35 +48,13 @@ def get_all_teams():
     return Teams.objects.order_by('name')
 
 
-# Redis HMSET for each match, using values() function to get mapping
-def set_redis_matches(matches):
-    for match in matches:
-        redis_match_key = 'match:{}'.format(match.id)
-        r.set(redis_match_key, match)
-
-
 # Get all the fields associated with a particular match_id
-def get_match(match_id):
-    return redis_get_all_fields(
-        key='match',
-        model=Matches(),
-        model_id=match_id
-    )
+def get_match(id):
+    return Matches.objects.get(id=id)
 
 
-# Redis HMSET for each boxscore, using values() function to get mapping
-def set_redis_boxscores(boxscores):
-    for boxscore in boxscores:
-        redis_boxscore_key = 'boxscore:{}'.format(boxscore.match_id)
-        r.set(redis_boxscore_key, boxscore)
-
-
-def get_boxscore(boxscore_id):
-    return redis_get_all_fields(
-        key='boxscore',
-        model=Boxscores(),
-        model_id=boxscore_id
-    )
+def get_boxscore(match_id):
+    return Boxscores.objects.get(match_id=match_id)
 
 
 def get_match_scores(match_id):
@@ -99,16 +71,4 @@ def get_latest_player_skills(player_id):
     return PlayerSkills.objects.latest_skills(
         pk=player_id
     )
-
-
-def redis_get_all_fields(key, model, model_id):
-    redis_item = r.get('{}:{}'.format(key, model_id))
-    if not redis_item:
-        if isinstance(model, Matches):
-            return Matches.objects.get(id=model_id)
-        if isinstance(model, Boxscores):
-            return Boxscores.objects.get(match_id=model_id)
-    else:
-        return redis_item
-
 
